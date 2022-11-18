749303ec3a92b65d6a3c5a77c9cd9307d6c9fe22
Stephen Halter <halter73@gmail.com>
2022-11-17 16:50:05 -0800

    WIP

--- RazorPlayground/Program.cs
+++ RazorPlayground/Program.cs
@@ -1,6 +1,28 @@
+using RazorLight;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var razorEngine = new RazorLightEngineBuilder()
+                //.UseEmbeddedResourcesProject(typeof(Program))
+                //.UseMemoryCachingProvider()
+                .Build();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/razor/{input}", (int input) => razorEngine.CompileRenderStringAsync("key1", """
+@for (var i = 0; i < @Model; i++)
+{ 
+    @: Hi #@i!
+}
+""", input));
+
+app.MapGet("/razor2/{input}", (int input) => razorEngine.CompileRenderStringAsync("key1", """
+@for (var i = 0; i < @Model; i++)
+{ 
+    @: Hello #@i!
+}
+""", input));
+
 
 app.Run();
